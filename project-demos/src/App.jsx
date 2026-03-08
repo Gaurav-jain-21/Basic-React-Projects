98761c64e33d758e9c0c6c50f7b1954ac431ee56
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-08 14:58:42 +0530

    this is my form validator

--- project-demos/src/App.jsx
+++ project-demos/src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import { accordionData } from "./utils/content";
-
+// import React from "react";
+// import Accordion from "./components/Accordion";
+// import { accordionData } from "./utils/content";
+import Form from "./components/Form";
 const App = () => {
   return (
-    <div>
-      <div className="accordion">
-        {accordionData.map(({ title, content }) => (
-          <Accordion title={title} content={content} />
-        ))}
-      </div>
-    </div>
+    <Form />
+    // <div>
+    //   <div className="accordion">
+    //     {accordionData.map(({ title, content }) => (
+    //       <Accordion title={title} content={content} />
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
